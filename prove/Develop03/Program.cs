0e81081684405d75a915a86014f2bbe9323c2457
Cecil Ewusie <Hendrixcecil0@gmail.com>
2024-05-15 00:46:04 +0000

    Modified program to improve the input

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -9,47 +9,62 @@ class Program
         //EXCEEDED REQUIREMENTS
         //Made the program so the user manually inputs their scripture they prefer to memorize
 
-        
+
 
         Console.WriteLine("WELCOME TO SCRIPTURE MEMORIZER APP");
 
-        Console.WriteLine("Press the enter key to continue or type 'quit' to end the program.");
-        string userInput = Console.ReadLine();      
+        Console.WriteLine(">>Press the enter key to continue or type 'quit' to end the program.");
+        string userInput = Console.ReadLine();
+
+        Console.WriteLine(">>Does this scripture include verse range eng, vrs 2-4, enter yes/no? "); 
+        string askVerseRange = Console.ReadLine();
+        
+        //declaring the scripture object outide of the if statement and assigning it to null at the moment
+        //value for the scripture object will be taken in the if block
+        Scripture scripture = null;    
 
         //having the user enter their choice of scripture to learn to memorize
-        Console.Write("Enter the name of the book you would like to memorize. example John, Proverbs etc? ");
+        Console.WriteLine(); //this is to provide space so output in terminal looks good
+        Console.Write(">>Enter the name of the book you would like to memorize. example John, Proverbs etc: ");
         string bookName = Console.ReadLine();
 
         //getting the chapter in interger
-        Console.WriteLine("Enter chapter, eg 12");
+        Console.WriteLine(); //this is to provide space so output in terminal looks good
+        Console.Write(">>Enter chapter, eg 12 :");
         string chapterNumber = Console.ReadLine();
         ConverString converString = new ConverString(chapterNumber);
         int chapterNumberConverted = converString.getInt();
 
         //getting the startVerse in integer
-        Console.WriteLine("Enter the start verse, eg 5 etc? ");
+        Console.WriteLine(); //this is to provide space so output in terminal looks good
+        Console.Write(">>Enter the start verse, eg 5 etc: ");
         string verse1 = Console.ReadLine();
         ConverString converString1 = new ConverString(verse1);
         int verse1int = converString1.getInt();
 
         //getting the text of the scripture
-        Console.WriteLine("Enter the whole text of the scripture, eg Jesus wept etc? ");
+        Console.WriteLine();
+        Console.WriteLine("Enter the whole text of the scripture, eg Jesus wept etc: ");
         string scriptText = Console.ReadLine();
 
         
-
+        if (askVerseRange == "no")
+        {
+            // declared the scripture object outside the while loop
+            scripture = new Scripture(new Reference(bookName, chapterNumberConverted, verse1int), scriptText); 
+        }
         
+        else if (askVerseRange == "yes")
+        {
+            Console.Write(">>Enter the end verse: ");
+            string verse2 = Console.ReadLine();
+            ConverString converString2 = new ConverString(verse2);
+            int verse2int = converString2.getInt();
 
-        Console.WriteLine("Enter the end verse? ");
-        string verse2 = Console.ReadLine();
-        ConverString converString2 = new ConverString(verse2);
-        int verse2int = converString2.getInt();
+            // declared the scripture object outside the while loop
+            scripture = new Scripture(new Reference(bookName, chapterNumberConverted, verse1int, verse2int), scriptText);             
+        }
 
-    
-
-
-        // declared the scripture object outside the while loop
-        Scripture scripture = new Scripture(new Reference(bookName, chapterNumberConverted, verse1int), scriptText); 
        
         //Scripture scripture = new Scripture(new Reference("John", 3, 16), "For God so loved the world...");        
 
