0b99545d68348e349c0471f66c62d116f992ad71
Cecil Ewusie <Hendrixcecil0@gmail.com>
2024-05-23 22:07:48 +0000

    finished learning about Poly

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 4);
+        Rectangle rectangle = new Rectangle(6, 4, "Green");
+        Circle circle = new Circle(2, "Grey");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape sh in shapes)
+        {
+            double disshape = sh.GetArea();
+            string discolor = sh.GetColor();
+
+            Console.WriteLine($"{discolor} color is {disshape} area.");
+        }
     }
 }
